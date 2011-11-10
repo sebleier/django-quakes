fd576e83319406ce2cc065e0e4f54de1eb5d4ef1
Sean Bleier <sebleier@gmail.com>
2011-11-10 11:42:48 -0800

    Added psycopg2 to the setup.py install_requires

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
     version="0.0.1",
     packages=["quakes"],
     description="A simple app to download and display USGS earthquake data",
-    install_requires=['django', 'redis>=2.4.5', 'django-celery', 'python-dateutil'],
+    install_requires=['django', 'redis>=2.4.5', 'django-celery', 'python-dateutil', 'psycopg2'],
     classifiers=[
         "Programming Language :: Python",
         "Operating System :: OS Independent",
